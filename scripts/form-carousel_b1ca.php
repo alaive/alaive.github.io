a2672c7a9bf121718c5955043728d3f2b2af90fa
alaive <71022493+alaive@users.noreply.github.com>
2021-01-28 20:50:46 +0100

    Add files via upload

---
+++ scripts/form-carousel_b1ca.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'email' => array(
+    'from' => 'aagosti@gwdg.de',
+    'to' => 'aagosti@gwdg.de'
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'address' => array(
+    'order' => 2,
+    'type' => 'string',
+    'label' => 'Address',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Address\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
